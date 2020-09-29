1caf827fa5a3d4de742e6f2619c3bf8b90ef5b0b
distant.zoomer <distantzoomer@protonmail.com>
2020-09-28 21:04:53 -0400

    Still trying

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 8080
 const bodyparser = require("body-parser")
-const webpack = require('webpack');
 const mustacheExpress = require( 'mustache-express' )
 const io = require( 'socket.io' )( 3000 )
 
